bd566400910cd1b235d6464ee5ad3c21275e0300
Ron Chittaro <ron@chittaro.com>
2022-12-02 13:28:57 -0500

    Chipping away at day 2

--- day2.go
+++ day2.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+var scoreMap = map[string]int{"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+
+func calculateMyScore(opponentPlay string, myPlay string) int {
+
+	baseScore := scoreMap[myPlay]
+
+}
 
 func Day_2() {
-	fmt.Println("Day 2")
+	fileIn, err := GetFileHandleByDay("2")
+
+	if err != nil {
+		panic("Input file expected")
+	}
+	defer fileIn.Close()
+
+	// Read line by line
+	s := bufio.NewScanner(fileIn)
+	for s.Scan() {
+
+		for i := 1; i <= 3; i++ {
+			roundPlay := strings.Fields(s.Text())
+
+			opponentPlay := roundPlay[0]
+			myPlay := roundPlay[1]
+
+			fmt.Println(opponentPlay, scoreMap[opponentPlay],
+				myPlay, scoreMap[myPlay])
+
+		}
+	}
 
 }
